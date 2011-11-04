38cab0b1cdaf171d974d47f67385a9376c3edb96
Simon Wistow <simon@thegestalt.org>
2011-11-03 18:11:25 -0700

    We need this to work against the https endpoint

--- fastly.php
+++ fastly.php
@@ -36,7 +36,7 @@ if (!function_exists('json_decode')) {
 add_option('fastly_hostname', '');
 add_option('fastly_api_key', '');
 add_option('fastly_service_id', '');
-add_option('fastly_api_hostname', 'api.fastly.com');
+add_option('fastly_api_hostname', 'ssl:api.fastly.com');
 add_option('fastly_api_port', 80);
 add_option('fastly_page', 'welcome');
 
