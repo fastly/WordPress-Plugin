e1b07fc898dd906c3e40d1e08a0d12348b5e589d
Simon Wistow <simon@thegestalt.org>
2011-11-28 11:34:10 -0800

    Don't include zips in the zip. Duh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 version=`cat "fastly.php" | grep '\@version' | perl -pe 's/.*\@version ([^ ]+).*$/$1/'`
 file="fastly-${version}.zip"
 rm "$file" >/dev/null 2>&1 
-zip -r "${file}" . -x "build.sh" -x ".*" 
+zip -r "${file}" . -x "build.sh" -x ".*" -x "*.zip"
