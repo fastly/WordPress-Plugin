c0627714b28aa78c7308518accf4731836f81022
Simon Wistow <simon@thegestalt.org>
2011-12-16 14:24:54 -0800

    Bump version to v0.8

--- fastly.php
+++ fastly.php
@@ -5,20 +5,20 @@ Plugin Name: Fastly
 Plugin URI: http://fastly.com/
 Description: Configuration and cache purging for the Fastly CDN.
 Author: Fastly.com
-Version: 0.7
+Version: 0.8
 Author URI: http://fastly.com/
 */
 
 /**
  * Main plugin code.
  * @package Fastly
- * @version 0.7
+ * @version 0.8
  * @author Ryan Sandor Richards
  * @copyright 2011 Fastly.com, All Rights Reserved
  */
 
 // Basic plugin definitions
-define('FASTLY_VERSION', '0.7');
+define('FASTLY_VERSION', '0.8');
 define('FASTLY_PLUGIN_URL', plugin_dir_url( __FILE__ ));
 
 // Includes
