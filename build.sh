db7faa31783ada800883d47e5f69177e3bb3fe0e
Ezekiel Templin <zeke@templ.in>
2014-10-02 05:48:26 -0700

    Update build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-version=`cat "fastly.php" | grep '\@version' | perl -pe 's/.*\@version ([^ ]+).*$/$1/'`
+version=`cat "fastly.php" | grep 'Version' | perl -pe 's/^.*Version: ([^ ]+).*$/$1/'`
 file="fastly-${version}.zip"
 rm "$file" >/dev/null 2>&1 
 zip -r "${file}" . -x "build.sh" -x ".*" -x "*.zip"
