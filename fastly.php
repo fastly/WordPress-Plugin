9c2af9ec534e080e732407ff75500d61a36f2798
Daniel McCarney <dmccarney@fastly.com>
2015-02-02 15:40:03 -0800

    Bumps version to 0.98 for security fixes

--- fastly.php
+++ fastly.php
@@ -5,20 +5,20 @@ Plugin Name: Fastly
 Plugin URI: http://fastly.com/
 Description: Configuration and cache purging for the Fastly CDN.
 Author: Fastly.com
-Version: 0.97
+Version: 0.98
 Author URI: http://fastly.com/
 */
 
 /**
  * Main plugin code.
  * @package Fastly
- * @version 0.97
+ * @version 0.98
  * @author Ryan Sandor Richards
  * @copyright 2011 Fastly.com, All Rights Reserved
  */
 
 // Basic plugin definitions
-define('FASTLY_VERSION', '0.97');
+define('FASTLY_VERSION', '0.98');
 define('FASTLY_PLUGIN_URL', plugin_dir_url( __FILE__ ));
 
 // Includes
